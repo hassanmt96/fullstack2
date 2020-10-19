42d13364608bf43c38d7444139172783db8f87b5
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-10-19 18:47:22 -0500

    phase4

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const { readFile } = require('fs').promises;
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 8081;
+
+const server = http.createServer(async(req, res) => {
+    if(req.url.startsWith('/images')){
+        const imageFilePath = './assets' + req.url;
+        const imageFileContents = await readFile(imageFilePath);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'image/jpeg')
+        res.end(imageFileContents);
+        return;
+    }
+        
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('I have items');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
